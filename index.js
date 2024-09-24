62e7e6b5156473c8d69939135691d529ee71bda5
saamuel <saamuel.auvaart@voco.ee>
2024-09-24 16:02:29 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./tasks", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const tasks = data.split("\n");
+      resolve(tasks);
+    });
+  });
+};
+
+app.post("/", (req, res) => {
+    console.log("form sent data");
+    res.render("index", { tasks: tasks });
+  });
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  //tasks list data from file
+  readFile("./tasks").then((tasks) => {
+    // add form sent task to array
+    tasks.push(req.body.task);
+    const data = tasks.join("\n");
+    fs.writeFile("./tasks", data, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // redirect to see result
+      res.redirect("/");
+    });
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Example app is started at http://localhost:3001");
+});
